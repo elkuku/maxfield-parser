ba1cb5a306c35d24f059a35d236c306714eb85e2
Nikolai Plath <der.el.kuku@gmail.com>
2022-02-18 11:37:59 -0500

    Refactor GpxHelper

--- src/GpxHelper.php
+++ src/GpxHelper.php
@@ -3,47 +3,121 @@
 namespace Elkuku\MaxfieldParser;
 
 use Elkuku\MaxfieldParser\Type\AgentLink;
+use Elkuku\MaxfieldParser\Type\KeyPrep;
+use Elkuku\MaxfieldParser\Type\LinkStep;
+use Elkuku\MaxfieldParser\Type\Waypoint;
 
 class GpxHelper
 {
-    public function getWaypointsGpx(string $item): string
+    public function getWaypointsGpx(MaxfieldParser $maxfieldParser): string
     {
-        $maxField = $this->maxFieldGenerator->getInfo($item);
-        $wayPoints = $this->maxFieldGenerator->parseWayPointsFile($item);
+        $keyPrep = $maxfieldParser->getKeyPrep();
+        $wayPoints = $maxfieldParser->parseWayPointsFile();
 
-        $xml = [];
-
-        $xml[] = $this->getGpxHeader();
-
-        foreach ($maxField->keyPrep->getWayPoints() as $wayPointAgent) {
-            $wayPoint = $wayPoints[$wayPointAgent->mapNo];
-            $xml[] = '<wpt lat="'.$wayPoint->getLat().'" lon="'
-                .$wayPoint->getLon().'">';
-            $xml[] = '  <name>'.$wayPoint->getName().'</name>';
-            $xml[] = '  <desc>Farm keys: '.$wayPointAgent->keysNeeded.'</desc>';
-            $xml[] = '</wpt>';
-        }
-
-        $xml[] = '</gpx>';
+        $xml = array_merge(
+            $this->getPartHeader(),
+            $this->getPartWaypoints($keyPrep, $wayPoints),
+            $this->getPartFooter(),
+        );
 
         return implode("\n", $xml);
     }
 
-
     public function getRouteTrackGpx(MaxfieldParser $maxfieldParser): string
     {
-        // $maxField = $this->maxFieldGenerator->getInfo($item);
-        // $wayPoints = $this->maxFieldGenerator->parseWayPointsFile($item);
-
-        $keyprepPoints = $maxfieldParser->getKeyPrep();
+        $keyPrep = $maxfieldParser->getKeyPrep();
         $wayPoints = $maxfieldParser->parseWayPointsFile();
         $links = $maxfieldParser->getLinks();
 
+        $xml = array_merge(
+            $this->getPartHeader(),
+            $this->getPartWaypoints($keyPrep, $wayPoints),
+            $this->getPartRoute($links, $wayPoints),
+            $this->getPartFooter(),
+        );
+
+        return implode("\n", $xml);
+    }
+
+    public function getRouteGpx(MaxfieldParser $maxfieldParser): string
+    {
+        $wayPoints = $maxfieldParser->parseWayPointsFile();
+        $links = $maxfieldParser->getLinks();
+
+        $xml = array_merge(
+            $this->getPartHeader(),
+            $this->getPartRoute($links, $wayPoints),
+            $this->getPartFooter(),
+        );
+
+        return implode("\n", $xml);
+    }
+
+    public function getTrackGpx(MaxfieldParser $maxfieldParser): string
+    {
+        $wayPoints = $maxfieldParser->parseWayPointsFile();
+        $links = $maxfieldParser->getLinks();
+
+        $xml = array_merge(
+            $this->getPartHeader(),
+            $this->getPartTrack($links, $wayPoints),
+            $this->getPartFooter(),
+        );
+
+        return implode("\n", $xml);
+    }
+
+    private function getPartHeader(): array
+    {
+        return [
+            '<?xml version="1.0" encoding="UTF-8"?>',
+            '<gpx version="1.0" creator="GPSBabel - http://www.gpsbabel.org" xmlns="http://www.topografix.com/GPX/1/0">',
+        ];
+    }
+
+    private function getPartFooter()
+    {
+        return ['</gpx>'];
+    }
+
+    /**
+     * @param AgentLink[] $links
+     * @param Waypoint[]  $wayPoints
+     */
+    private function getPartTrack(array $links, array $wayPoints): array
+    {
+        $steps = $this->calculateSteps($links);
+
         $xml = [];
 
-        $xml[] = $this->getGpxHeader();
+        $xml[] = '<trk>';
+        $xml[] = '<name>Route name</name>';
+        $xml[] = '<trkseg>';
 
-        foreach ($keyprepPoints->getWayPoints() as $wayPointAgent) {
+        foreach ($steps as $step) {
+            $origin = $wayPoints[$step->origin];
+            $xml[] = '<trkpt lat="'.$origin->lat.'"'
+                .' lon="'.$origin->lon.'">';
+            $xml[] = '<name>'.$origin->name.'</name>';
+            $desc = implode(', ', $step->getDestinations());
+            $xml[] = '<desc>'.$desc.'</desc>';
+            $xml[] = '</trkpt>';
+        }
+
+        $xml[] = '</trkseg>';
+        $xml[] = '</trk>';
+
+        return $xml;
+    }
+
+    /**
+     * @param Waypoint[] $wayPoints
+     */
+    private function getPartWaypoints(KeyPrep $keyPrep, array $wayPoints): array
+    {
+        $xml = [];
+
+        foreach ($keyPrep->getWayPoints() as $wayPointAgent) {
             $wayPoint = $wayPoints[$wayPointAgent->mapNo];
             $xml[] = '<wpt lat="'.$wayPoint->lat.'" lon="'
                 .$wayPoint->lon.'">';
@@ -52,128 +126,67 @@ class GpxHelper
             $xml[] = '</wpt>';
         }
 
+        return $xml;
+    }
+
+    /**
+     * @param AgentLink[] $links
+     * @param Waypoint[]  $waypoints
+     */
+    private function getPartRoute(array $links, array $waypoints): array
+    {
+        $xml = [];
+
         $xml[] = '<rte>';
         $xml[] = '<name>Routenname</name>';
 
         $steps = $this->calculateSteps($links);
 
         foreach ($steps as $step) {
-            $origin = $wayPoints[$step->origin];
+            $origin = $waypoints[$step->origin];
             $xml[] = '<rtept lat="'.$origin->lat.'"'
                 .' lon="'.$origin->lon.'">';
             $xml[] = '<name>'.$origin->name.'</name>';
-            $links = '';
-            foreach ($step->destinations as $index) {
-                $links .= 'Link: '.$wayPoints[$index]->name.'*BR*';
+            $linksString = '';
+            foreach ($step->getDestinations() as $index) {
+                $linksString .= 'Link: '.$waypoints[$index]->name.'*BR*';
             }
 
-            $xml[] = '<desc>'.$links.'</desc>';
+            $xml[] = '<desc>'.$linksString.'</desc>';
             $xml[] = '</rtept>';
         }
 
         $xml[] = '</rte>';
 
-        $xml[] = '</gpx>';
-
-        return implode("\n", $xml);
-    }
-
-
-    public function getRouteGpx(string $item): string
-    {
-        $maxField = $this->maxFieldGenerator->getInfo($item);
-        $wayPoints = $this->maxFieldGenerator->parseWayPointsFile($item);
-
-        $steps = $this->calculateSteps($maxField->links);
-
-        $xml = [];
-
-        $xml[] = $this->getGpxHeader();
-
-        $xml[] = '<rte>';
-        $xml[] = '<name>Routenname</name>';
-
-        foreach ($steps as $step) {
-            $origin = $wayPoints[$step->origin];
-            $xml[] = '<rtept lat="'.$origin->getLat().'"'
-                .' lon="'.$origin->getLon().'">';
-            $xml[] = '<name>'.$origin->getName().'</name>';
-            $desc = implode(', ', $step->destinations);
-            $xml[] = '<desc>'.$desc.'</desc>';
-            $xml[] = '</rtept>';
-        }
-
-        $xml[] = '</rte>';
-
-        $xml[] = '</gpx>';
-
-        return implode("\n", $xml);
-    }
-
-    public function getTrackGpx(string $item): string
-    {
-        $maxField = $this->maxFieldGenerator->getInfo($item);
-        $wayPoints = $this->maxFieldGenerator->parseWayPointsFile($item);
-
-        $steps = $this->calculateSteps($maxField->links);
-
-        $xml = [];
-
-        $xml[] = $this->getGpxHeader();
-
-        $xml[] = '<trk>';
-        $xml[] = '<name>Routenname</name>';
-        $xml[] = '<trkseg>';
-
-        foreach ($steps as $step) {
-            $origin = $wayPoints[$step->origin];
-            $xml[] = '<trkpt lat="'.$origin->getLat().'"'
-                .' lon="'.$origin->getLon().'">';
-            $xml[] = '<name>'.$origin->getName().'</name>';
-            $desc = implode(', ', $step->destinations);
-            $xml[] = '<desc>'.$desc.'</desc>';
-            $xml[] = '</trkpt>';
-        }
-
-        $xml[] = '</trkseg>';
-        $xml[] = '</trk>';
-
-        $xml[] = '</gpx>';
-
-        return implode("\n", $xml);
+        return $xml;
     }
 
     /**
      * @param AgentLink[] $links
+     *
+     * @return LinkStep[]
      */
     private function calculateSteps(array $links): array
     {
+        /**
+         * @var LinkStep[] $steps
+         */
         $steps = [];
         $index = -1;
-        $origin = '';
+        $origin = 0;
+
         foreach ($links as $link) {
             if ($link->originNum !== $origin) {
                 $index++;
-                $step = new \stdClass();
-                $step->origin = $link->originNum;
-                $step->destinations = [];
-
-                $step->destinations[] = $link->destinationNum;
-
-                $steps[$index] = $step;
                 $origin = $link->originNum;
+
+                $steps[$index] = (new LinkStep($link->originNum))
+                    ->addtDestination($link->destinationNum);
             } else {
-                $steps[$index]->destinations[] = $link->destinationNum;
+                $steps[$index]->addtDestination($link->destinationNum);
             }
         }
 
         return $steps;
-    }
-
-    private function getGpxHeader(): string
-    {
-        return '<?xml version="1.0" encoding="UTF-8"?>'
-            .'<gpx version="1.0" creator="GPSBabel - http://www.gpsbabel.org"'
-            .' xmlns="http://www.topografix.com/GPX/1/0">';
     }
 }
