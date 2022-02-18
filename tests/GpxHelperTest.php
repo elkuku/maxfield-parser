9c747ae466b4d445015bcdd12d1351e789205bef
Nikolai Plath <der.el.kuku@gmail.com>
2022-02-18 12:34:25 -0500

    Introducing an error :)

--- tests/GpxHelperTest.php
+++ tests/GpxHelperTest.php
@@ -12,60 +12,44 @@ class GpxHelperTest extends TestCase
 
     public function testGetRouteTrackGpx(): void
     {
-        $gpxExpected = trim(
-            file_get_contents($this->testDir.'/test.gpx') ?: ''
-        );
+        $gpxExpected = trim(file_get_contents($this->testDir.'/test.gpx'));
 
         $gpxHelper = new GpxHelper();
 
-        $gpx = $gpxHelper->getRouteTrackGpx(
-            new MaxfieldParser($this->testDir.'/12345')
-        );
+        $gpx = $gpxHelper->getRouteTrackGpx(new MaxfieldParser($this->testDir.'/12345'));
 
         self::assertEquals($gpxExpected, $gpx);
     }
 
     public function testGetWaypointsGpx(): void
     {
-        $gpxExpected = trim(
-            file_get_contents($this->testDir.'/test_waypoints.gpx') ?: ''
-        );
+        $gpxExpected = trim(file_get_contents($this->testDir.'/test_waypoints.gpx')?:'');
 
         $gpxHelper = new GpxHelper();
 
-        $gpx = $gpxHelper->getWaypointsGpx(
-            new MaxfieldParser($this->testDir.'/12345')
-        );
+        $gpx = $gpxHelper->getWaypointsGpx(new MaxfieldParser($this->testDir.'/12345'));
 
         self::assertEquals($gpxExpected, $gpx);
     }
 
     public function testGetRouteGpx(): void
     {
-        $gpxExpected = trim(
-            file_get_contents($this->testDir.'/test_route.gpx') ?: ''
-        );
+        $gpxExpected = trim(file_get_contents($this->testDir.'/test_route.gpx')?:'');
 
         $gpxHelper = new GpxHelper();
 
-        $gpx = $gpxHelper->getRouteGpx(
-            new MaxfieldParser($this->testDir.'/12345')
-        );
+        $gpx = $gpxHelper->getRouteGpx(new MaxfieldParser($this->testDir.'/12345'));
 
         self::assertEquals($gpxExpected, $gpx);
     }
 
     public function testGetTrackGpx(): void
     {
-        $gpxExpected = trim(
-            file_get_contents($this->testDir.'/test_track.gpx') ?: ''
-        );
+        $gpxExpected = trim(file_get_contents($this->testDir.'/test_track.gpx')?:'');
 
         $gpxHelper = new GpxHelper();
 
-        $gpx = $gpxHelper->getTrackGpx(
-            new MaxfieldParser($this->testDir.'/12345')
-        );
+        $gpx = $gpxHelper->getTrackGpx(new MaxfieldParser($this->testDir.'/12345'));
 
         self::assertEquals($gpxExpected, $gpx);
     }
